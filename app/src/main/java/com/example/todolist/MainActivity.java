7b69a734007662df938fc518d4d2aaf0426c2dc5
99002514 <amrita.singh@ltts.com>
2020-11-06 17:24:20 +0530

    commit by 99002441 added recycler views

--- app/src/main/java/com/example/todolist/MainActivity.java
+++ app/src/main/java/com/example/todolist/MainActivity.java
@@ -24,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
+//
         joinNowButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
